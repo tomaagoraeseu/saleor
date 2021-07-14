c8c2f7dc7be67caf1978580478f09ac2b9b88643
kuchichan <pawel.kucharski@mirumee.com>
2021-07-14 09:04:07 +0200

    Add filtering capability for 'is_private' and 'click_and_collect_option' fields in warehouse

--- saleor/tests/fixtures.py
+++ saleor/tests/fixtures.py
@@ -4317,6 +4317,7 @@ def warehouses_for_cc(address, shipping_zones):
                 slug="warehouse3",
                 email="warehouse3@example.com",
                 click_and_collect_option=WarehouseClickAndCollectOption.LOCAL_STOCK,
+                is_private=False,
             ),
             Warehouse(
                 address=address.get_copy(),
@@ -4324,6 +4325,7 @@ def warehouses_for_cc(address, shipping_zones):
                 slug="warehouse4",
                 email="warehouse4@example.com",
                 click_and_collect_option=WarehouseClickAndCollectOption.LOCAL_STOCK,
+                is_private=False,
             ),
         ]
     )
